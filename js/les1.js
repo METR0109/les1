78864f8914ca94ea1648ac56bd5e34aa18492be3
METR0109 <110671168+METR0109@users.noreply.github.com>
2024-03-24 15:30:43 +0200

    t5

--- js/les1.js
+++ js/les1.js
@@ -37,3 +37,68 @@
 // firstName.length > 4 && lastName.length > 5
 //   ? console.log(totalLetters)
 //   : console.log('неможливо виконати умову');
+
+//TODO: 3 ===================================
+// Написати скрипт, який перевіряє чи введене число через prompt потрапляє в діапазон від 55 до 99 включно.
+// Якщо так, вивести в консоль "Число потрапляє в діапазон".
+// Якщо ні, вивести в консоль "Число не потрапляє в діапазон".
+
+// const num = prompt('Enter number');
+// alert(num);
+
+// if (num >= 55 && num <= 99) {
+//   alert('Число потрапляє в діапазон');
+// } else {
+//   alert('Число не потрапляє в діапазон');
+// }
+
+// const num = prompt('Enter number');
+// alert(num);
+// num >= 55 && num <= 99
+//   ? alert('Число потрапляє в діапазон')
+//   : alert('Число не потрапляє в діапазон');
+
+//TODO: 4 ===================================
+// Є 3 вікові групи : діти (0-16), дорослі(17-60), пенсіонери (61-100).
+// До якої групи відноситься Максим, якщо йому 55 років.Назву групи вивести на екран.
+
+// const maksymAge = 61;
+// if (maksymAge >= 0 && maksymAge <= 16) {
+//   console.log('діти');
+// } else if (maksymAge > 16 && maksymAge <= 60) {
+//   console.log('дорослі');
+// } else if (maksymAge >= 61 && maksymAge <= 100) {
+//   console.log('пенсіонери');
+// } else {
+//   console.log('eror');
+// }
+
+//TODO: 5 ===================================
+//* Конструкція switch
+// Запитай користувача у модальному вікні, який з напоїв він бажає придбати - "Кава", "Чай" чи "Сік",
+// та залежно від виду напою оголоси суму досплати.Використовувати switch.
+
+// const drink = prompt('Choice drink').toLowerCase();
+
+// function orderDrink(drinkName) {
+//   let price;
+//   switch (drinkName) {
+//     case 'coffe':
+//       price = 40;
+//       break;
+
+//     case 'tea':
+//       price = 25;
+//       break;
+
+//     case 'joce':
+//       price = 50;
+//       break;
+
+//     default:
+//       alert('error');
+//   }
+//   return `Your order total ${price} UAH`;
+// }
+
+// alert(orderDrink(drink));
