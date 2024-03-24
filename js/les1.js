87ec9a2da4120e102d71bcaa880d5449d5a28934
METR0109 <110671168+METR0109@users.noreply.github.com>
2024-03-24 16:58:24 +0200

    Update les1.js

--- js/les1.js
+++ js/les1.js
@@ -102,3 +102,109 @@
 // }
 
 // alert(orderDrink(drink));
+
+//* Цикл while and do while
+// TODO: 6 ===================================
+// Напишіть цикл, який пропонує ввести
+// число більше 100 через prompt.
+// Якщо відвідувач ввів інше число - попросити
+// ввести ще раз і так далі.
+// Цикл має питати число, поки відвідувач не
+// введе число більше 100, або натисне кнопку
+// скасування в prompt
+
+// let number = Number(prompt('число більше 100'));
+// console.log('r -> number', number);
+// while (Number(number) <= 100 || number !== null) {
+//   number = Number(prompt('число більше 100'));
+// }
+
+// let number = Number(prompt('число більше 100'));
+// console.log('r -> number', number);
+// do {
+//   //   number = Number(prompt('число більше 100'));
+//   if (number === null) {
+//     alert('Cancel');
+//     break;
+//   }
+//   number = prompt('число більше 100');
+// } while (Number(number) <= 100);
+
+// console.log('r -> number', number);
+
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// do {
+//   console.log('hello');
+// } while (true);
+
+//TODO: 7 ===================================
+// Знайдіть число 49  у проміжку чисел від 1 до 100 та вийдіть з циклу.
+
+// const targetNumber = 49;
+// for (let i = 1; i <= 100; i += 1) {
+//   console.log('r - >tatargetNumber', targetNumber);
+//   break;
+// }
+
+//TODO: 8 ===================================
+//  Виведіть методом console.log() зірочки від 1 до 7 штук у вигляді трикутника таким чино
+// *
+// **
+// ***
+// ****
+// *****
+
+// let star = '*';
+// for (let i = 1; i <= 7; i += 1) {
+//   console.log(star);
+//   star += '*';
+// }
+
+//TODO: 9 ===================================
+// Порахуйте суму чисел від 12 до 27
+
+// const min = 12;
+// const max = 27;
+// let sum = 0;
+// for (let i = min;)
+
+//TODO: 10 ===================================
+//Якщо число ділитися на 3 повертати fizz
+//якщо ділитися на 5 повертати buzz
+//Якщо ділитися на 3 і на 5 повернути fizzbuzz
+
+// function checkNumber(num) {
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else if (i % 3 === 0) {
+//       console.log('fizz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+// checkNumber(30);
+
+// function checkNumber(num) {
+//   for (let i = 1; i <= num; i++) {
+//     let str = '';
+//     if (i % 3 === 0) {
+//       str += 'fizz';
+//     }
+
+//     if (i % 5 === 0) {
+//       str += 'buzz';
+//     }
+
+//     console.log(str || i);
+//   }
+// }
+// checkNumber(30);
